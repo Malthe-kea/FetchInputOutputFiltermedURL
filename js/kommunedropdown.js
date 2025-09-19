abca0c6f8cab57920c8b12e5ede4cc9ebf5a23ad
malthe-kea <mati0002@stud.kea.dk>
2025-09-19 09:25:24 +0200

    Kan vise navn og kode på kommuner fra DD

--- js/kommunedropdown.js
+++ js/kommunedropdown.js
@@ -1,29 +1,42 @@
 console.log("kommundropdown.js er her")
 let pbFetchKommuner = document.getElementById("pbFetchKommuner");
-let ddKommune = document.getElementById("ddKommuner");
-let selectedKommune = ddKommune.addEventListener("change", goToHref)
+let ddSelectedKommune = document.getElementById("ddKommuner");
+const kommuneMap = new Map();
+const infoHolder = document.getElementById("info")
 pbFetchKommuner.addEventListener("click", actionFetch);
 
-const urlKommune = "https://api.dataforsyningen.dk/kommuner";
+const url = "https://api.dataforsyningen.dk/kommuner";
 
 function fetchKommuner(any) {
     return fetch(any).then(response => response.json());
 }
 
 async function actionFetch() {
-    const kommuner = (await fetchKommuner(urlKommune)).sort((a, b) => a.navn.localeCompare(b.navn));
-    ddKommune.innerHTML = ""; // Clear existing options
-    kommuner.forEach(kommune => {
+    const kommuner = (await fetchKommuner(url)).sort((a, b) => a.navn.localeCompare(b.navn));
+    ddSelectedKommune.innerHTML = ""; // Clear existing options
+    for (let kom of kommuner) {
         const option = document.createElement("option");
-        option.value = kommune.kode;
-        option.textContent = kommune.navn;
-        ddKommune.appendChild(option);
-    });
-}
+        option.value = kom.kode;
+        option.textContent = kom.navn;
+        ddSelectedKommune.appendChild(option);
 
-function goToHref(){
-    const selectedValue = ddKommune.value;
-    if(selectedValue){
-        window.location.href = selectedValue
+        kommuneMap.set(kom.kode, kom)
     }
 }
+
+
+function showKommuneInfo(kommune) {
+        infoHolder.innerHTML = "";
+        const navn = document.createElement("h1")
+        navn.textContent = kommune.navn
+        infoHolder.appendChild(navn)
+
+        const kode = document.createElement("h2")
+        kode.textContent = kommune.kode
+        infoHolder.appendChild(kode);
+        updateMap(kommune)
+}
+ddSelectedKommune.addEventListener("change", function () {
+    const kommune = kommuneMap.get(ddSelectedKommune.value);
+    showKommuneInfo(kommune);
+});
